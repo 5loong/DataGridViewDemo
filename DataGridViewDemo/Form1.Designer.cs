04f4988fa28a8e4b8286632a8813245faf069605
5loong <godloong.com@gmail.com>
2013-05-12 23:15:19 +0800

    The first version of the demo

---
+++ DataGridViewDemo/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DataGridViewDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_list = new System.Windows.Forms.DataGridView();
+            this.btn_test = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_read = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_list)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_list
+            // 
+            this.dgv_list.AllowUserToAddRows = false;
+            this.dgv_list.AllowUserToResizeRows = false;
+            this.dgv_list.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgv_list.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_list.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgv_list.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgv_list.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgv_list.Location = new System.Drawing.Point(0, 0);
+            this.dgv_list.Name = "dgv_list";
+            this.dgv_list.RowHeadersVisible = false;
+            this.dgv_list.RowTemplate.Height = 23;
+            this.dgv_list.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgv_list.Size = new System.Drawing.Size(502, 272);
+            this.dgv_list.TabIndex = 0;
+            this.dgv_list.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dgv_list_EditingControlShowing);
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(114, 312);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 1;
+            this.btn_test.Text = "TEST";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "选择";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "序号";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "种类";
+            this.Column3.Items.AddRange(new object[] {
+            "XXU",
+            "BKP",
+            "FAN",
+            "POWER"});
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "子类型";
+            this.Column4.Items.AddRange(new object[] {
+            "NULL",
+            "PEM0",
+            "PEM1",
+            "PEM2",
+            "PEM3"});
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "文件名";
+            this.Column5.MinimumWidth = 50;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(305, 312);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(75, 23);
+            this.btn_read.TabIndex = 2;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 382);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.btn_test);
+            this.Controls.Add(this.dgv_list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_list)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_list;
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button btn_read;
+    }
+}
+
---
+++ DataGridViewDemo/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataGridViewDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgv_list.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            //add one row ,combobox column's value must in its list, checkbox column's value can be set as bool
+
+            dgv_list.Rows.Add(true,"21000000", "XXU", "NULL", "lpu1");
+
+        }
+
+        private void dgv_list_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            ComboBox cb = e.Control as ComboBox;
+            if (cb != null)
+            {
+                // first remove event handler to keep from attaching multiple:
+                cb.SelectedIndexChanged -= new
+                EventHandler(cb_SelectedIndexChanged);
+
+                // now attach the event handler
+                cb.SelectedIndexChanged += new
+                EventHandler(cb_SelectedIndexChanged);
+            }
+        }
+
+        void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selected index changed");
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            string str = "";
+
+            foreach (DataGridViewRow dgvr in dgv_list.Rows)
+            {
+                if ((bool)dgvr.Cells[0].FormattedValue)
+                {
+                    str += dgvr.Cells[1].FormattedValue;
+                }
+            }
+            MessageBox.Show(str);
+        }
+
+    }   
+}
